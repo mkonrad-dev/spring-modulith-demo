ef4025ead924ec41cd0d730bbe5b8e11538296c7
Markus Konrad <mkonrad.dev@gmail.com>
2023-04-22 20:04:01 +0200

    Attempt to fix the cycle detection using Modulithic annotation

--- src/main/kotlin/org/example/modulithdemo/ModulithDemoApplication.kt
+++ src/main/kotlin/org/example/modulithdemo/ModulithDemoApplication.kt
@@ -2,8 +2,10 @@ package org.example.modulithdemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.modulith.Modulithic
 
 @SpringBootApplication
+@Modulithic(additionalPackages = ["org.example.modulithdemo.modules"], useFullyQualifiedModuleNames = true)
 class ModulithDemoApplication
 
 fun main(args: Array<String>) {
--- src/test/kotlin/org/example/modulithdemo/CustomApplicationModuleDetectionStrategy.kt
+++ src/test/kotlin/org/example/modulithdemo/CustomApplicationModuleDetectionStrategy.kt
@@ -4,7 +4,7 @@ import org.springframework.modulith.core.ApplicationModuleDetectionStrategy
 import org.springframework.modulith.core.JavaPackage
 import java.util.stream.Stream
 
-class CustomApplicationModuleDetectionStrategy: ApplicationModuleDetectionStrategy {
+class CustomApplicationModuleDetectionStrategy : ApplicationModuleDetectionStrategy {
 
     /**
      * Identifies the following two modules:
@@ -13,10 +13,11 @@ class CustomApplicationModuleDetectionStrategy: ApplicationModuleDetectionStrate
      *
      * Check target/spring-modulith-docs/components.puml after running ApplicationModuleTest.kt
      */
-    override fun getModuleBasePackages(basePackage: JavaPackage): Stream<JavaPackage> {
-        return when(basePackage.name) {
-            "org.example.modulithdemo" -> basePackage.directSubPackages.stream().flatMap { getModuleBasePackages(it) }
-            else -> basePackage.directSubPackages.stream()
+    override fun getModuleBasePackages(basePackage: JavaPackage): Stream<JavaPackage> =
+        when (basePackage.name) {
+            "org.example.modulithdemo.modules" -> basePackage.directSubPackages.stream().filter {
+                it.name == "org.example.modulithdemo.modules.one" || it.name == "org.example.modulithdemo.modules.two"
+            }
+            else -> Stream.empty()
         }
-    }
 }
